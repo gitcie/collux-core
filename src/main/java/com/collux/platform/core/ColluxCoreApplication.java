da78851e66d88321125ab73c3247736c48e8a0a8
lusiyi@szboanda.net <lusiyi@szboanda.net>
2020-08-17 23:27:50 +0800

    init commit

---
+++ src/main/java/com/collux/platform/core/ColluxCoreApplication.java
@@ -0,0 +1,13 @@
+package com.collux.platform.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColluxCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColluxCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collux/platform/core/ColluxCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collux.platform.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColluxCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
