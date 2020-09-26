f36699d0108ac49abcbcf6ba63f016c2469e80fb
lusiyi@szboanda.net <lusiyi@szboanda.net>
2020-09-27 06:23:12 +0800

    add eureka config

--- collux-polestar/src/main/kotlin/com/collux/platform/polestar/ColluxPolestarApplication.kt
+++ collux-polestar/src/main/kotlin/com/collux/platform/polestar/ColluxPolestarApplication.kt
@@ -2,8 +2,10 @@ package com.collux.platform.polestar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 open class ColluxPolestarApplication
 
 fun main(args: Array<String>) {
